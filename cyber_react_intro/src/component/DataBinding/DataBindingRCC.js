e30e6245e7c923b54a2e9a2033332320e9941650
danh tran <tldanh1993@gmail.com>
2021-12-08 20:49:06 +0700

    render method, function w return rule for rcc. rfc

--- cyber_react_intro/src/component/DataBinding/DataBindingRCC.js
+++ cyber_react_intro/src/component/DataBinding/DataBindingRCC.js
@@ -27,6 +27,38 @@ export default class DataBindingRCC extends Component {
     name: 'Dark Vader',
     charClass: 'lord',
   };
+
+  // Binding phương thức (hàm của lớp đối tượng)
+  // Tất cả hàm render đều phải trả về component (jsx) hoặc số / chuổi / boolean
+  // Gọi render thì có thêm ()
+  renderImage = () => {
+    // img ở đây là jsx - 1 component của react và ctrl-click vào dc
+    return (
+      <img
+        src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+        alt="img render"
+      />
+    );
+  };
+
+  renderInfoVirus = () => {
+    const virus = {
+      id: 'covid-19',
+      name: 'corona',
+      img: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+      alias: 'SARS-CoV-2',
+    };
+    return (
+      <div className="card text-white bg-primary w-25 h-25">
+        <img className="card-img-top" src={virus.img} alt={virus.id} />
+        <div className="card-body">
+          <h4 className="card-title">{virus.name}</h4>
+          <p className="card-text">{virus.alias}</p>
+        </div>
+      </div>
+    );
+  };
+
   render() {
     // Trong hàm (method) chỉ có biến
     // Biến trong đây local ko share ra ngoài scope
@@ -37,6 +69,26 @@ export default class DataBindingRCC extends Component {
       name: 'Yoda',
       charClass: 'teacher',
     };
+
+    // Nếu để bên trong thì là hàm, gọi ko cần this ~ rfc
+    const renderInfoVirusFunc = () => {
+      const virus = {
+        id: 'covid-19',
+        name: 'corona',
+        img: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+        alias: 'SARS-CoV-2',
+      };
+      return (
+        <div className="card text-white bg-secondary w-25 h-25">
+          <img className="card-img-top" src={virus.img} alt={virus.id} />
+          <div className="card-body">
+            <h4 className="card-title">{virus.name}</h4>
+            <p className="card-text">{virus.alias}</p>
+          </div>
+        </div>
+      );
+    };
+
     return (
       <div>
         <h2>Data Binding RCC - class</h2>
@@ -49,6 +101,10 @@ export default class DataBindingRCC extends Component {
         <h4 className="text-warning">
           Student of {character2.charClass} - {character2.name}
         </h4>
+        <hr></hr>
+        {this.renderImage()}
+        {this.renderInfoVirus()}
+        {renderInfoVirusFunc()}
       </div>
     );
   }
