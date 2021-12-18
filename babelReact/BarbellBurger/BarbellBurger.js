ed8e8d7569851cec70a335f30c16c56d90e211ce
danh tran <tldanh1993@gmail.com>
2021-12-18 13:16:50 +0700

    render burger display and total sum

--- babelReact/BarbellBurger/BarbellBurger.js
+++ babelReact/BarbellBurger/BarbellBurger.js
@@ -12,6 +12,19 @@ class BarbellBurger extends React.Component {
     };
   }
 
+  renderMidBurger = () => {
+    let temp = this.state.burger.map(({ type, amount }, index) => {
+      let midBurger = [];
+      for (let index = 0; index < amount; index++) {
+        midBurger.push(<div key={type + index} className={type}></div>);
+      }
+      return midBurger;
+    });
+    console.log('temp:', temp);
+    // Array nested-array vẫn trả về jsx render bình thường
+    return temp;
+  };
+
   render() {
     return (
       <div className="container">
@@ -20,9 +33,7 @@ class BarbellBurger extends React.Component {
           <div className="col-7">
             <h2 className="text-center">Current Burger</h2>
             <div className="breadTop"></div>
-            <div className="salad"></div>
-            <div className="cheese"></div>
-            <div className="beef"></div>
+            {this.renderMidBurger()}
             <div className="breadBottom"></div>
           </div>
           <div className="col-5">
@@ -38,19 +49,43 @@ class BarbellBurger extends React.Component {
               <tbody>
                 <tr>
                   <td className="text-capitalize">salad</td>
-                  <td>1</td>
+                  <td>
+                    <button className="btn btn-success mr-2" onClick={() => {}}>
+                      +
+                    </button>
+                    1
+                    <button className="btn btn-danger ml-2" onClick={() => {}}>
+                      -
+                    </button>
+                  </td>
                   <td>10</td>
                   <td>10</td>
                 </tr>
                 <tr>
                   <td className="text-capitalize">cheese</td>
-                  <td>1</td>
+                  <td>
+                    <button className="btn btn-success mr-2" onClick={() => {}}>
+                      +
+                    </button>
+                    1
+                    <button className="btn btn-danger ml-2" onClick={() => {}}>
+                      -
+                    </button>
+                  </td>
                   <td>20</td>
                   <td>20</td>
                 </tr>
                 <tr>
                   <td className="text-capitalize">beef</td>
-                  <td>1</td>
+                  <td>
+                    <button className="btn btn-success mr-2" onClick={() => {}}>
+                      +
+                    </button>
+                    1
+                    <button className="btn btn-danger ml-2" onClick={() => {}}>
+                      -
+                    </button>
+                  </td>
                   <td>55</td>
                   <td>55</td>
                 </tr>
@@ -59,7 +94,11 @@ class BarbellBurger extends React.Component {
                 <tr>
                   <td colSpan="2"></td>
                   <td>Sum</td>
-                  <td>85</td>
+                  <td>
+                    {this.state.burger.reduce((prev, { amount, price }) => {
+                      return (prev += amount * price);
+                    }, 0)}
+                  </td>
                 </tr>
               </tfoot>
             </table>
