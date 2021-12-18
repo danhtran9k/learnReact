aa652689063ed46c85331b0a1a469e90e5343c7b
danh tran <tldanh1993@gmail.com>
2021-12-18 12:53:01 +0700

    burger static layout w table

--- babelReact/BarbellBurger/BarbellBurger.js
+++ babelReact/BarbellBurger/BarbellBurger.js
@@ -11,11 +11,60 @@ class BarbellBurger extends React.Component {
       ],
     };
   }
-  
+
   render() {
     return (
       <div className="container">
         <h1 className="text-center">Barbell Burger</h1>
+        <div className="row text-center">
+          <div className="col-7">
+            <h2 className="text-center">Current Burger</h2>
+            <div className="breadTop"></div>
+            <div className="salad"></div>
+            <div className="cheese"></div>
+            <div className="beef"></div>
+            <div className="breadBottom"></div>
+          </div>
+          <div className="col-5">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Type</th>
+                  <th>Amount</th>
+                  <th>Price</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="text-capitalize">salad</td>
+                  <td>1</td>
+                  <td>10</td>
+                  <td>10</td>
+                </tr>
+                <tr>
+                  <td className="text-capitalize">cheese</td>
+                  <td>1</td>
+                  <td>20</td>
+                  <td>20</td>
+                </tr>
+                <tr>
+                  <td className="text-capitalize">beef</td>
+                  <td>1</td>
+                  <td>55</td>
+                  <td>55</td>
+                </tr>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="2"></td>
+                  <td>Sum</td>
+                  <td>85</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
