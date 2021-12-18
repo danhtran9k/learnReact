c0755e8a8a0409e5a5c1bc79ef23a090f212a949
danh tran <tldanh1993@gmail.com>
2021-12-18 12:23:01 +0700

    > Babel - Barbell Burger rcc snippet init

---
+++ babelReact/BarbellBurger/BarbellBurger.js
@@ -0,0 +1,14 @@
+// rcc snippet init
+import React, { Component } from 'react';
+
+export default class BarbellBurger extends Component {
+  render() {
+    return ( 
+        <div>
+            Barbell Burger
+        </div>
+    )
+  }
+}
+
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
