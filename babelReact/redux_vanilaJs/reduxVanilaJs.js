32adec1ce35b2e44d6204865c264e7a09bc23da9
danh tran <tldanh1993@gmail.com>
2022-01-01 10:33:21 +0700

    setup state - reducer - store

--- babelReact/redux_vanilaJs/reduxVanilaJs.js
+++ babelReact/redux_vanilaJs/reduxVanilaJs.js
@@ -1,1 +1,30 @@
-console.log(window.Redux)
+console.log(window.Redux);
+// method
+// Trong mô hình redux store bọc lớn nhất -> create cuối
+// Tạo những phần con unit nhỏ nhất trước
+// SETUP REDUX STORE
+// state
+// reducer
+// store
+
+// Ở đây State init ta dùng thẳng 1 array ko xài obj vì bài toàn đơn giản
+const reduxVanilaJsState = ['initial state'];
+
+const reduxVanilaJsReducer = (stateInit = reduxVanilaJsState, action) => {
+  const state = { ...stateInit };
+
+  switch (action.type) {
+    case 'ADD_HOBBY':
+      state.__dataChange = action.payload;
+      return state;
+    default:
+      return stateInit;
+  }
+};
+
+// export default reduxVanilaJsReducer;
+
+// Create store
+const { createStore } = window.Redux;
+const store = createStore(reduxVanilaJsReducer);
+console.log('store:', store);
