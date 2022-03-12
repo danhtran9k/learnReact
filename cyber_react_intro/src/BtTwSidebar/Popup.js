c4902304e42751123d4f6d556141d7867a0ad32a
danh tran <tldanh1993@gmail.com>
2022-03-12 15:44:02 +0700

    Demo tailwind modal w state and modal animation

--- cyber_react_intro/src/BtTwSidebar/Popup.js
+++ cyber_react_intro/src/BtTwSidebar/Popup.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import _ from 'lodash';
 import './Popup.css';
-export default function Popup() {
+export default function Popup(props) {
   //   https://tailwindui.com/components/application-ui/overlays/modals#component-47a5888a08838ad98779d50878d359b3
   //  1 số component trên tailwindui.com có tính phí
+  const { openModal, setOpenModal, item } = props;
+
+  const classModal = [
+    'fixed',
+    'z-10',
+    'inset-0',
+    'overflow-y-auto',
+    openModal ? 'openModal' : 'closeModal',
+  ];
 
   return (
     <div
-      className="fixed z-10 inset-0 overflow-y-auto"
+      className={_.join(classModal, ' ')}
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
@@ -68,14 +77,11 @@ Leaving: "ease-in duration-200"
                   className="text-lg leading-6 font-medium text-gray-900"
                   id="modal-title"
                 >
-                  item.tenMonAn
+                  {item.tenMonAn}
                 </h3>
                 <div className="mt-2">
                   <p className="text-sm text-gray-500">
-                    <img
-                      src="https://source.unsplash.com/100x100/?portrait"
-                      className="w-100"
-                    />
+                    <img src={item.hinhAnh} className="w-100" />
                   </p>
                 </div>
               </div>
@@ -83,6 +89,9 @@ Leaving: "ease-in duration-200"
           </div>
           <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <button
+              onClick={() => {
+                setOpenModal(false);
+              }}
               type="button"
               className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
             >
