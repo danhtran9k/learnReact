077ce9196c74cdbc8072d9a058e3e3edf9884009
danh tran <tldanh1993@gmail.com>
2022-03-12 15:44:02 +0700

    Demo tailwind modal w state and modal animation

--- cyber_react_intro/src/BtTwSidebar/Popup.js
+++ cyber_react_intro/src/BtTwSidebar/Popup.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import _ from 'lodash';
 import './Popup.css';
-export default function Popup() {
+export default function Popup(props) {
   //   https://tailwindui.com/components/application-ui/overlays/modals#component-47a5888a08838ad98779d50878d359b3
   //  1 số component trên tailwindui.com có tính phí
+  const { openModal, setOpenModal, item } = props;
+
+  const classModal = [
+    'fixed',
+    'z-10',
+    'inset-0',
+    'overflow-y-auto',
+    openModal ? 'openModal' : 'closeModal',
+  ];
 
   return (
     <div
-      className="fixed z-10 inset-0 overflow-y-auto"
+      className={_.join(classModal, ' ')}
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
     >
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        {/*
-Background overlay, show/hide based on modal state.
-
-Entering: "ease-out duration-300"
-  From: "opacity-0"
-  To: "opacity-100"
-Leaving: "ease-in duration-200"
-  From: "opacity-100"
-  To: "opacity-0"
-    */}
+      {/*
+        Background overlay, show/hide based on modal state.
+        Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+        Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+      */}
         <div
           className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
           aria-hidden="true"
@@ -32,16 +40,15 @@ Leaving: "ease-in duration-200"
           className="hidden sm:inline-block sm:align-middle sm:h-screen"
           aria-hidden="true"
         ></span>
-        {/*
-Modal panel, show/hide based on modal state.
-
-Entering: "ease-out duration-300"
-  From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-  To: "opacity-100 translate-y-0 sm:scale-100"
-Leaving: "ease-in duration-200"
-  From: "opacity-100 translate-y-0 sm:scale-100"
-  To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-    */}
+      {/*
+        Modal panel, show/hide based on modal state.
+        Entering: "ease-out duration-300"
+        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+        From: "opacity-100 translate-y-0 sm:scale-100"
+        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      */}
         <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
           <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
@@ -68,13 +75,14 @@ Leaving: "ease-in duration-200"
                   className="text-lg leading-6 font-medium text-gray-900"
                   id="modal-title"
                 >
-                  item.tenMonAn
+                  {item.tenMonAn}
                 </h3>
                 <div className="mt-2">
                   <p className="text-sm text-gray-500">
                     <img
-                      src="https://source.unsplash.com/100x100/?portrait"
+                      src={item.hinhAnh}
                       className="w-100"
+                      alt={item.tenMonAn}
                     />
                   </p>
                 </div>
@@ -83,6 +91,9 @@ Leaving: "ease-in duration-200"
           </div>
           <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <button
+              onClick={() => {
+                setOpenModal(false);
+              }}
               type="button"
               className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
             >
